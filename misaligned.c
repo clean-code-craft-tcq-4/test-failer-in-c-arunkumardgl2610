fb3fd5bbc8d3d393878691c6a4e0b59678683428
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-06-24 13:18:40 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -6,14 +6,14 @@ int printColorMap()
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i=0,j=0;
-    for(i = 1; i < 5; i++) 
+    for(i = 0; i < 5; i++) 
     {
         for(j = 0; j < 5; j++) 
         {
-            printf("%d ->Major color = %s & Minor color = %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            printf("%d ->Major color = %s & Minor color = %s\n", (i * 5 + j), majorColor[i], minorColor[j]);
         }
     }
-    return i * j;
+    return i + j;
 }
 
 int main() {
