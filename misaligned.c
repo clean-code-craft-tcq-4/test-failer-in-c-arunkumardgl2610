476e9dad5ae00c356a71f4b201753bd509c35d35
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-06-30 16:22:20 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <assert.h>
 
-int printColorMap() 
+int printColorMap(unsigned int Get_Max_colour ) 
 {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i=0,j=0;
-    for(i = 0; i < 5; i++) 
+    for(i = 0; i < Get_Max_colour; i++) 
     {
-        for(j = 0; j < 5; j++) 
+        for(j = 0; j < Get_Max_colour; j++) 
         {
-            printf("%d ->Major color = %s & Minor color = %s\n", (i * 5 + j), majorColor[i], minorColor[j]);
+            printf("%d ->Major color = %s & Minor color = %s\n", (i * Get_Max_colour + j), majorColor[i], minorColor[j]);
         }
     }
     return i + j;
 }
 
-int main() {
-    int result = printColorMap();
+int main() 
+{
+    int result = printColorMap(5);
     assert(result == 25);
     printf("All is well (maybe!)\n");
     return 0;
