532e3b485d882d37c218d2a142ff3fa4802e9117
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-07-04 16:51:49 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -5,15 +5,15 @@ char size(int shoulder_length, int Body_length) //Length in CM
 {
     char sizeName = '\0';
     
-    if((shoulder_length < 38)  && (Body_length <60))
+    if((shoulder_length <= 38)  && (Body_length <= 60))
     {
         sizeName = 'S';
     } 
-    else if((shoulder_length >= 38 && shoulder_length < 42) && (Body_length >= 60 && Body_length < 72))
+    else if((shoulder_length > 38 && shoulder_length <= 42) && (Body_length > 60 && Body_length <= 72))
     {
         sizeName = 'M';
     } 
-    else if((shoulder_length >= 42) && (Body_length >= 72) )
+    else if((shoulder_length > 42) && (Body_length > 72) )
     {
         sizeName = 'L';
     }
@@ -21,10 +21,8 @@ char size(int shoulder_length, int Body_length) //Length in CM
 }
 
 int main() {
-    assert(size(38,60) == 'S'); 
-    assert(size(37,60) == 'S'); //Tshirt size is derived based on shoulder length and body length.
+    assert(size(38,60) == 'S'); //Tshirt size is derived based on shoulder length and body length.
     assert(size(40,65) == 'M');
-    assert(size(42,60) == 'S'); 
     assert(size(43,74) == 'L');
     printf("All is well (maybe!)\n");
     return 0;
